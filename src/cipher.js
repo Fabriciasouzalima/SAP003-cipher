10b26bab7346b5d8c8c33a6bf99e4be699a3a12d
Fabriciasouzalima <fabriciasouzalima@gmail.com>
2019-08-20 17:45:01 -0300

    adicionar link entre html e index.js

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,11 @@
 window.cipher = {
   // ... 
 };
+
+//encode (msg e deslocamento){}
+
+
+//decode (msg e deslocamento){}
+
+// fórmula 
+// (letra em ascii - posição da letra + valor fixo)% 26(número de letras)+65
--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-// eventos do dom
+// function save input
+
+function cipherEnc()
+{event.preventDefault() 
+let mensagem = document.getElementById("mSecret").value
+let keyCode = document.getElementById("btn-numbercipher").value
+console.log(mensagem)
+console.log(keyCode)                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
+// cipher.encode ()
+}
+                                                
+function cipherDec()
+{event.preventDefault()
+let mensagem2= document.getElementById("deSecret").value
+let keyCode = document.getElementById("btn-numbercipher").value
+console.log(mensagem2)
+console.log(keyCode)
+// cipher.decode ()
+}
+
+
+// innerHtml
