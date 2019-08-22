390600b0ebd0601f83c85819c8ebea13b28a1a31
Fabriciasouzalima <fabriciasouzalima@gmail.com>
2019-08-22 17:15:44 -0300

    cifrar o código para tratar minúsculas e caracteres

--- src/cipher.js
+++ src/cipher.js
@@ -1,25 +1,24 @@
 window.cipher = {
   cipherEncode: cipherEncode,
   cipherDecode: cipherDecode,
-  
+
 };
 
 function cipherEncode(mensagem, keyCode) {
   let msgResult = '';
-  for (let i=0; i<mensagem.length; i++) {
-    console.log (mensagem.charCodeAt(i), keyCode)
-    msgResult += String.fromCharCode(((mensagem.charCodeAt(i)- 65) + keyCode)% 26 + 65);
+  for (let i = 0; i < mensagem.length; i++) {
+    msgResult += String.fromCharCode(((mensagem.charCodeAt(i) - 65) + keyCode) % 26 + 65);
   }
   return msgResult;
-} 
-  
-  
-  
+}
+
+
+
 function cipherDecode(mensagem2, keyCode) {
   let msgResult2 = '';
-  for (let i=0; i<mensagem2.length; i++) {
-    msgResult2 += String.fromCharCode(((mensagem2.charCodeAt(i)- 65) - keyCode) % 26 + 65);
+  for (let i = 0; i < mensagem2.length; i++) {
+    msgResult2 += String.fromCharCode(((mensagem2.charCodeAt(i) - 65) - keyCode) % 26 + 65);
   }
   return msgResult2;
-} 
+}
 
