21d82b4e69dd75329238fc108cd8028c1389c8f3
Fabriciasouzalima <fabriciasouzalima@gmail.com>
2019-08-23 12:37:52 -0300

    tratei maiusculas e minusculas

--- src/cipher.js
+++ src/cipher.js
@@ -4,10 +4,20 @@ window.cipher = {
 
 };
 
+/* if(){
+
+}*/
+
 function cipherEncode(mensagem, keyCode) {
   let msgResult = '';
   for (let i = 0; i < mensagem.length; i++) {
-    msgResult += String.fromCharCode(((mensagem.charCodeAt(i) - 65) + keyCode) % 26 + 65);
+    if (mensagem.charCodeAt(i) >= 65 && mensagem.charCodeAt(i) <= 90) {
+      msgResult += String.fromCharCode(((mensagem.charCodeAt(i) - 65) + keyCode) % 26 + 65);
+    }
+    else if(mensagem.charCodeAt(i) >= 97 && mensagem.charCodeAt(i) <=122){
+      msgResult += String.fromCharCode(((mensagem.charCodeAt(i) - 97) + keyCode) % 26 + 97)
+    }
+
   }
   return msgResult;
 }
@@ -17,7 +27,13 @@ function cipherEncode(mensagem, keyCode) {
 function cipherDecode(mensagem2, keyCode) {
   let msgResult2 = '';
   for (let i = 0; i < mensagem2.length; i++) {
-    msgResult2 += String.fromCharCode(((mensagem2.charCodeAt(i) - 65) - keyCode) % 26 + 65);
+    if (mensagem2.charCodeAt(i) >= 65 && mensagem2.charCodeAt(i) <= 90) {
+      msgResult2 += String.fromCharCode(((mensagem2.charCodeAt(i) - 65) - keyCode) % 26 + 65)
+    }
+    else if (mensagem2.charCodeAt(i) >= 97 && mensagem2.charCodeAt(i)<=122){
+      msgResult2 += String.fromCharCode(((mensagem2.charCodeAt(i) - 97) - keyCode) % 26 + 97)
+    }
+
   }
   return msgResult2;
 }
