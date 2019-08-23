28c6cbcf7259c6089901e40dfb55a74b989021b9
Fabriciasouzalima <fabriciasouzalima@gmail.com>
2019-08-23 16:05:55 -0300

    debugar linhas do codigo

--- src/cipher.js
+++ src/cipher.js
@@ -4,36 +4,41 @@ window.cipher = {
 
 };
 
-/* if(){
-
-}*/
-
 function cipherEncode(mensagem, keyCode) {
-  let msgResult = '';
+  let msgResult = "";
   for (let i = 0; i < mensagem.length; i++) {
-    if (mensagem.charCodeAt(i) >= 65 && mensagem.charCodeAt(i) <= 90) {
+    if (mensagem.charCodeAt(i) == 32) {
+      msgResult += "";
+    }
+    else if (mensagem.charCodeAt(i) >= 65 && mensagem.charCodeAt(i) <= 90) {
       msgResult += String.fromCharCode(((mensagem.charCodeAt(i) - 65) + keyCode) % 26 + 65);
     }
-    else if(mensagem.charCodeAt(i) >= 97 && mensagem.charCodeAt(i) <=122){
-      msgResult += String.fromCharCode(((mensagem.charCodeAt(i) - 97) + keyCode) % 26 + 97)
+    else if (mensagem.charCodeAt(i) >= 97 && mensagem.charCodeAt(i) <= 122) {
+      msgResult += String.fromCharCode(((mensagem.charCodeAt(i) - 97) + keyCode) % 26 + 97);
+    }
+    else {
+      msgResult += mensagem[i];
     }
 
   }
   return msgResult;
 }
 
-
-
 function cipherDecode(mensagem2, keyCode) {
-  let msgResult2 = '';
+  let msgResult2 = " ";
   for (let i = 0; i < mensagem2.length; i++) {
-    if (mensagem2.charCodeAt(i) >= 65 && mensagem2.charCodeAt(i) <= 90) {
-      msgResult2 += String.fromCharCode(((mensagem2.charCodeAt(i) - 65) - keyCode) % 26 + 65)
+    if (mensagem2.charCodeAt(i) == 32) {
+      msgResult2 += "";
     }
-    else if (mensagem2.charCodeAt(i) >= 97 && mensagem2.charCodeAt(i)<=122){
-      msgResult2 += String.fromCharCode(((mensagem2.charCodeAt(i) - 97) - keyCode) % 26 + 97)
+    else if (mensagem2.charCodeAt(i) >= 65 && mensagem2.charCodeAt(i) <= 90) {
+      msgResult2 += String.fromCharCode(((mensagem2.charCodeAt(i) - 65) - keyCode) % 26 + 65);
     }
-
+    else if (mensagem2.charCodeAt(i) >= 97 && mensagem2.charCodeAt(i) <= 122) {
+      msgResult2 += String.fromCharCode(((mensagem2.charCodeAt(i) - 97) - keyCode) % 26 + 97);
+    }
+    else {
+      msgResult2 += mensagem2[i];
+    }
   }
   return msgResult2;
 }
--- src/index.js
+++ src/index.js
@@ -1,60 +1,17 @@
-function cipherEnc(){
-  event.preventDefault() 
+function cipherEnc() {
+  event.preventDefault();
   let mensagem = document.getElementById("mSecret").value;
   let keyCode = parseInt(document.getElementById("deslocamento").value);
-  let final = window.cipher.cipherEncode(mensagem,keyCode);
+  let final = window.cipher.cipherEncode(mensagem, keyCode);
   document.getElementById("encode").innerHTML = final;
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              
-}
 
-
-                                                
+}	
+                                               
 function cipherDec() {
-	event.preventDefault()
-	let mensagem2 = document.getElementById("deSecret").value;
-	let keyCode = document.getElementById("deslocamento").value;
-	let final2 = window.cipher.cipherDecode(mensagem2, keyCode);
-	document.getElementById("decode").innerHTML = final2;
+  event.preventDefault();
+  let mensagem2 = document.getElementById("deSecret").value;
+  let keyCode = document.getElementById("deslocamento").value;
+  let final2 = window.cipher.cipherDecode(mensagem2, keyCode);
+  document.getElementById("decode").innerHTML = final2;
+
 }
-
-
-
-/*function limpaString (str) {
-	str = str.toLowerCase();
-
-    str = replaceAll("é", "e", str);
-	str = replaceAll("ê", "e", str);
-	str = replaceAll("ẽ", "e", str);
-	str = replaceAll("è", "e", str);
-	str = replaceAll("ë", "e", str);
-
-	str = replaceAll("ã", "a", str);
-	str = replaceAll("á", "a", str);
-	str = replaceAll("à", "a", str);
-	str = replaceAll("â", "a", str);
-	str = replaceAll("ä", "a", str);
-
-	str = replaceAll("ó", "o", str);
-	str = replaceAll("õ", "o", str);
-	str = replaceAll("ô", "o", str);
-	str = replaceAll("ò", "o", str);
-	str = replaceAll("ö", "o", str);
-
-	str = replaceAll("í", "i", str);
-	str = replaceAll("ì", "i", str);
-	str = replaceAll("î", "i", str);
-	str = replaceAll("ĩ", "i", str);
-    str = replaceAll("ï", "i", str);
-
-    str = replaceAll("ú", "u", str);
-	str = replaceAll("û", "u", str);
-	str = replaceAll("ù", "u", str);
-	str = replaceAll("ũ", "u", str);
-	str = replaceAll("ü", "u", str);
-
-	str = replaceAll("ç", "c", str);
-
-	str = replaceAll("ñ", "n", str);
-
-	return str;
-}*/ 
