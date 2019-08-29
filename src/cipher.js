cfb9080bf8d4395e76c8410077dfbf66f6ed4aed
Fabriciasouzalima <fabriciasouzalima@gmail.com>
2019-08-29 14:28:51 -0300

    implementar offset negativo

--- src/cipher.js
+++ src/cipher.js
@@ -8,10 +8,10 @@ function cipherEncode(keyCode, mensagem) {
   for (let i = 0; i < mensagem.length; i++) {
     
     if (mensagem.charCodeAt(i) >= 65 && mensagem.charCodeAt(i) <= 90) {
-      msgResult += String.fromCharCode(((mensagem.charCodeAt(i) - 65) + keyCode) % 26 + 65);
+      msgResult += String.fromCharCode(((mensagem.charCodeAt(i) - 65) + (keyCode %26+26)) % 26 + 65);
     }
     else if (mensagem.charCodeAt(i) >= 97 && mensagem.charCodeAt(i) <= 122) {
-      msgResult += String.fromCharCode(((mensagem.charCodeAt(i) - 97) + keyCode) % 26 + 97);
+      msgResult += String.fromCharCode(((mensagem.charCodeAt(i) - 97) + (keyCode%26+26)) % 26 + 97);
     }
     else {
       msgResult += mensagem[i];
@@ -37,5 +37,3 @@ function cipherDecode(keyCode, mensagem2) {
   }
   return msgResult2;
 }
-
-
